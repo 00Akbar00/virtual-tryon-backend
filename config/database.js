0f304aa2b195e8229732da6f8351f1932317a213
00Akbar00 <ma341604@gmail.com>
2025-03-19 20:32:50 +0500

    prisma package added for db management

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
-const mogoose = require('mongoose')
+const mogoose = require("mongoose");
 
-const { DB_CON_STRING } = process.env
+const { DB_CON_STRING } = process.env;
 
 module.exports = () => {
-    //mogoose.connect("mongodb://localhost/test")
-    mogoose.connect("mongodb+srv://Akbar:123123123@cluster0.pjzvswh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        .then(() => console.log('DB Connection Successfull'))
-        .catch(err => console.log(err.message))
-}
+  //mogoose.connect("mongodb://localhost/test")
+  mogoose
+    .connect(
+      "mongodb+srv://Akbar:123123123@cluster0.pjzvswh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => console.log("DB Connection Successfull"))
+    .catch((err) => console.log(err.message));
+};
